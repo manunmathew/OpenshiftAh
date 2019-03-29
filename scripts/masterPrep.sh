2844c77a53f8d6cd3af842efdf0c7f9dae765aac
Harold Wong <harold.wong@microsoft.com>
2019-03-28 20:33:13 -0700

    don't attach pool id

--- scripts/masterPrep.sh
+++ scripts/masterPrep.sh
@@ -40,20 +40,20 @@ else
 	fi
 fi
 
-subscription-manager attach --pool=$POOL_ID > attach.log
-if [ $? -eq 0 ]
-then
-    echo "Pool attached successfully"
-else
-    grep attached attach.log
-    if [ $? -eq 0 ]
-    then
-        echo "Pool $POOL_ID was already attached and was not attached again."
-    else
-        echo "Incorrect Pool ID or no entitlements available"
-        exit 4
-   fi
-fi
+# subscription-manager attach --pool=$POOL_ID > attach.log
+# if [ $? -eq 0 ]
+# then
+    # echo "Pool attached successfully"
+# else
+    # grep attached attach.log
+    # if [ $? -eq 0 ]
+    # then
+        # echo "Pool $POOL_ID was already attached and was not attached again."
+    # else
+        # echo "Incorrect Pool ID or no entitlements available"
+        # exit 4
+   # fi
+# fi
 
 # Disable all repositories and enable only the required ones
 echo $(date) " - Disabling all repositories and enabling only the required repos"
